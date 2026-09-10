9346bc224e724dc4174e92cbdfe4518e779249a2
JMartinez-Helios <jjulve2003@gmail.com>
2026-09-10 11:07:03 +0200

    Cambios siguiendo la directriz de IT

--- aws_deply.py
+++ aws_deply.py
@@ -5,10 +5,10 @@ import os
 # TODO: Borrar estas credenciales hardcodeadas antes de que vengan los auditores. 
 # Mover a variables de entorno en el servidor.
 
-AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE2026"
-AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-INTERNAL_DC_IP = "10.50.2.10"
-AWS_REGION = "eu-south-2"
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+INTERNAL_DC_IP = os.getenv("INTERNAL_DC_IP")
+AWS_REGION = os.getenv("AWS_REGION")
 
 def deploy_infra():
     print("Iniciando despliegue automatizado en Nodo Espartal...")
--- nota.md
+++
@@ -1,2 +0,0 @@
-Nota para IT:
-Deberíamos cambiar la contraseña por defecto, el nombre de mi perro y el año de mi ingreso no parece muy seguro
